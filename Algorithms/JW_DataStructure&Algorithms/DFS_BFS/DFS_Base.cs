e7741a97ef8674e06b873bdc1217b35a72745ab8
JaeWoo Joe <joejw7554@gmail.com>
2026-02-07 20:32:23 +0900

    DFS 기본 형태 공부 완료

---
+++ Algorithms/JW_DataStructure&Algorithms/DFS_BFS/DFS_Base.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using System.Text;
+
+
+class DFS_Base
+{
+    static bool[] visited = new bool[10];
+
+    static List<int>[] graph = new List<int>[10];
+
+    static Stack<int> VisitOrder = new Stack<int>();
+
+
+
+    static void DFS(int start)
+    {
+        visited[start] = true;
+        VisitOrder.Push(start);
+
+        while (VisitOrder.Count > 0)
+        {
+            if (VisitOrder.TryPop(out int current))
+            {
+
+                //visited[current] = true;
+
+
+                for (int i = graph[current].Count - 1; i >= 0; i--)
+                {
+                    var nextNode = graph[current][i];
+                    if (visited[nextNode] == false)
+                    {
+                        visited[nextNode] = true;
+                        VisitOrder.Push(nextNode);
+
+                        Console.WriteLine("Push: " + nextNode);
+                    }
+                }
+            }
+        }
+
+    }
+
+
+    static void Main()
+    {
+        for (int i = 0; i < graph.Length; i++) graph[i] = new List<int>();
+
+        graph[1].Add(2);
+        graph[1].Add(3);
+
+        graph[2].Add(3);
+
+
+        DFS(1);
+
+    }
+
+
+}
--- Algorithms/JW_DataStructure&Algorithms/Greedy/P_42862.cs
+++ Algorithms/JW_DataStructure&Algorithms/Greedy/P_42862.cs
@@ -1,101 +1,101 @@
-﻿//using System.Collections.Generic;
+﻿////using System.Collections.Generic;
+////using System.Text;
+////using System;
+
+//////https://school.programmers.co.kr/learn/courses/30/lessons/42862
+////public class Solution
+////{
+////    public int solution(int n, int[] lost, int[] reserve)
+////    {
+////        int answer = n;
+
+////        Array.Sort(lost);
+////        Array.Sort(reserve);
+
+////        //reserve와 lost 동시에 있는 학생 찾아서 -1로 표기
+////        for (int i = 0; i < reserve.Length; i++)
+////        {
+////            for (int j = 0;j < lost.Length; j++)
+////                {
+////                    if (lost[j] == reserve[i]) 
+////                    {
+////                        lost[j] = -1;
+////                        reserve[i] = -1;
+////                        break;
+////                    }
+////                }
+
+////        }
+
+
+////        //reserve학생들의 인접한 한생 빌려주기
+////        foreach (var item in reserve)
+////        {
+////            for (int i = 0; i < lost.Length; i++)
+////            {
+////                if (item == -1) continue;//자기가 입어야했던 애들은 패스
+
+////                if ((item + 1 == lost[i]) || (item - 1 == lost[i])) //여벌이 있는 애들
+////                {
+////                    lost[i] = -1;
+////                    break;
+////                }
+////            }
+////        }
+
+////        int count = lost.Count((x => x > 0));
+////        answer -= count;
+
+////        return answer;
+////    }
+
+////        //반복문 끝난뒤에 n-count하면 값이 나올꺼같다
+////    static void Main()
+////    {
+////        var sol = new Solution();
+////        sol.solution(5, new int[] { 2, 4 }, new int[] { 1, 3, 5 });
+////    }
+////}
+
+
+
+//using System.Collections.Generic;
 //using System.Text;
 //using System;
+//using System.Linq;
 
 ////https://school.programmers.co.kr/learn/courses/30/lessons/42862
 //public class Solution
 //{
 //    public int solution(int n, int[] lost, int[] reserve)
 //    {
-//        int answer = n;
+//        var lostHash = lost.ToHashSet();
+//        var reserveHash = reserve.ToHashSet();
 
-//        Array.Sort(lost);
-//        Array.Sort(reserve);
+//        var lostStudent = reserve.Intersect(lostHash);
 
-//        //reserve와 lost 동시에 있는 학생 찾아서 -1로 표기
-//        for (int i = 0; i < reserve.Length; i++)
+//        reserveHash.ExceptWith(lostStudent);
+//        lostHash.ExceptWith(lostStudent);
+
+//        foreach (var item in reserveHash.Order())
 //        {
-//            for (int j = 0;j < lost.Length; j++)
-//                {
-//                    if (lost[j] == reserve[i]) 
-//                    {
-//                        lost[j] = -1;
-//                        reserve[i] = -1;
-//                        break;
-//                    }
-//                }
-
-//        }
-
-
-//        //reserve학생들의 인접한 한생 빌려주기
-//        foreach (var item in reserve)
-//        {
-//            for (int i = 0; i < lost.Length; i++)
+//            if (lostHash.Contains(item + 1))
 //            {
-//                if (item == -1) continue;//자기가 입어야했던 애들은 패스
-
-//                if ((item + 1 == lost[i]) || (item - 1 == lost[i])) //여벌이 있는 애들
-//                {
-//                    lost[i] = -1;
-//                    break;
-//                }
+//                lostHash.Remove(item + 1);
+//            }
+//            else if (lostHash.Contains(item - 1))
+//            {
+//                lostHash.Remove(item - 1);
 //            }
 //        }
 
-//        int count = lost.Count((x => x > 0));
-//        answer -= count;
-
-//        return answer;
+//        return n - lostHash.Count;
 //    }
 
-//        //반복문 끝난뒤에 n-count하면 값이 나올꺼같다
+
 //    static void Main()
 //    {
 //        var sol = new Solution();
-//        sol.solution(5, new int[] { 2, 4 }, new int[] { 1, 3, 5 });
+//        Console.WriteLine(sol.solution(5, new int[] { 1, 4 }, new int[] { 1, 3, 5 })); // 5
 //    }
 //}
-
-
-
-using System.Collections.Generic;
-using System.Text;
-using System;
-using System.Linq;
-
-//https://school.programmers.co.kr/learn/courses/30/lessons/42862
-public class Solution
-{
-    public int solution(int n, int[] lost, int[] reserve)
-    {
-        var lostHash = lost.ToHashSet();
-        var reserveHash = reserve.ToHashSet();
-
-        var lostStudent = reserve.Intersect(lostHash);
-
-        reserveHash.ExceptWith(lostStudent);
-        lostHash.ExceptWith(lostStudent);
-
-        foreach (var item in reserveHash.Order())
-        {
-            if (lostHash.Contains(item + 1))
-            {
-                lostHash.Remove(item + 1);
-            }
-            else if (lostHash.Contains(item - 1))
-            {
-                lostHash.Remove(item - 1);
-            }
-        }
-
-        return n - lostHash.Count;
-    }
-
-
-    static void Main()
-    {
-        var sol = new Solution();
-        Console.WriteLine(sol.solution(5, new int[] { 1, 4 }, new int[] { 1, 3, 5 })); // 5
-    }
-}
