02e8c681d9ecc9a9a4c26aacb827ad4ff3626ff5
JaeWoo Joe <joejw7554@gmail.com>
2026-03-01 16:46:17 +0900

    Greedy 큰수만들기 문제 풀이 완료

---
+++ Algorithms/JW_DataStructure&Algorithms/Greedy/P_42883.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+//https://school.programmers.co.kr/learn/courses/30/lessons/42883
+class P_42883
+{
+    public string solution(string number, int k)
+    {
+        string answer = "";
+        int removeCount = 0;
+
+        Stack<char> stack = new Stack<char>();
+
+        foreach (var item in number)
+        {
+            //지금 스택의 Top이 item 보다 큰지 확인해서 끝까지 다 뺀다
+            //RemoveCount가 K보다 작아야한다
+            //빈 stack이 아니어야한다
+            while (stack.Count > 0 &&
+                   removeCount < k&&  
+                   stack.Peek() < item  )
+            {
+                stack.Pop();
+                removeCount++;
+            }
+
+            stack.Push(item);
+        }
+
+       
+        while(removeCount<k)
+        {
+            stack.Pop();
+            removeCount++;
+        }
+
+        StringBuilder sb;
+        char[] temp = stack.ToArray();
+        Array.Reverse(temp);
+
+        answer = new string(temp);
+
+        return answer;
+    }
+}
