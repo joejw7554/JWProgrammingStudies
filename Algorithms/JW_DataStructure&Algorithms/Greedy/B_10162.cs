c6c98d4d318e69722a76e5af29984af6d089444b
JaeWoo Joe <joejw7554@gmail.com>
2026-03-02 18:19:51 +0900

    Greedy 문제 풀이 완료

--- Algorithms/JW_DataStructure&Algorithms/Greedy/B_10162.cs
+++ Algorithms/JW_DataStructure&Algorithms/Greedy/B_10162.cs
@@ -1,37 +1,37 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Text;
 
-//https://www.acmicpc.net/problem/10162
-class B_10162
-{
-    static void Main()
-    {
-        var input = int.Parse(Console.ReadLine());
+////https://www.acmicpc.net/problem/10162
+//class B_10162
+//{
+//    static void Main()
+//    {
+//        var input = int.Parse(Console.ReadLine());
     
 
-        int A = 300;
-        int B = 60;
-        int C = 10;
+//        int A = 300;
+//        int B = 60;
+//        int C = 10;
 
-        int[] timers = new int[3] { 300, 60, 10 };
-        List<int> counts = new List<int>();
+//        int[] timers = new int[3] { 300, 60, 10 };
+//        List<int> counts = new List<int>();
 
-        foreach (int num in timers)
-        {
-            int count = input / num;
-            counts.Add(count);
-            input -= num * count;
-        }
+//        foreach (int num in timers)
+//        {
+//            int count = input / num;
+//            counts.Add(count);
+//            input -= num * count;
+//        }
 
-        if (input != 0)
-        {
-            Console.WriteLine(-1);
-        }
-        else
-        {
+//        if (input != 0)
+//        {
+//            Console.WriteLine(-1);
+//        }
+//        else
+//        {
 
-            Console.Write(string.Join(" ", counts));
-        }
-    }
-}
+//            Console.Write(string.Join(" ", counts));
+//        }
+//    }
+//}
---
+++ Algorithms/JW_DataStructure&Algorithms/Greedy/B_5585.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+
+//https://www.acmicpc.net/problem/5585
+
+class B_5585
+{
+    static void Main()
+    {
+        var input = int.Parse(Console.ReadLine());
+        int remian = 1000 - input;
+        int answer = 0;
+
+        int[] changes = { 500, 100, 50, 10, 5, 1 };
+
+        for(int i=0; i<changes.Length;i++)
+        {
+            answer += remian / changes[i];
+            remian = remian % changes[i];
+        }
+
+        Console.WriteLine(answer);
+
+    }
+}
