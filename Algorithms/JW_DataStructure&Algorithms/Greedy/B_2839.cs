80389d04ce0de5980774030f26f904262a415d67
JaeWoo Joe <joejw7554@gmail.com>
2026-03-03 08:19:06 +0900

    B2839 풀이완료

---
+++ Algorithms/JW_DataStructure&Algorithms/Greedy/B_2839.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+//https://www.acmicpc.net/problem/2839
+
+class B_2839
+{
+    static void Main()
+    {
+        int input = int.Parse(Console.ReadLine());
+
+        for (int fiveKG = input / 5; fiveKG >= 0; fiveKG--)
+        {
+            int remain = input - (5 * fiveKG);
+
+            if (remain % 3 == 0)
+            {
+                Console.WriteLine(fiveKG + remain / 3);
+                return;
+            }
+        }
+
+
+        Console.WriteLine(-1);
+    }
+
+
+}
