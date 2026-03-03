8a696959d42fc7b7815688eaa6d73cd23117b867
JaeWoo Joe <joejw7554@gmail.com>
2026-03-03 10:50:39 +0900

    11047 풀이 완료

--- Algorithms/JW_DataStructure&Algorithms/Greedy/B_11047.cs
+++ Algorithms/JW_DataStructure&Algorithms/Greedy/B_11047.cs
@@ -1,10 +1,31 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Text;
 
-class B_11047
-{
-    
+////https://www.acmicpc.net/problem/11047
+//class B_11047
+//{
+//    static void Main()
+//    {
+//        int[] inputs = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+//        int N = inputs[0], K = inputs[1];
 
+//        int[] coinTypes = new int[N];
+//        for(int i=0; i<N; i++)
+//        {
+//            coinTypes[i]= int.Parse(Console.ReadLine());
+//        }
 
-}
+//        Array.Sort(coinTypes, (a, b) => b.CompareTo(a));
+
+//        int count = 0;
+//        foreach(int type in coinTypes)
+//        {
+//            count += K / type;
+//            K = K % type;
+//        }
+
+//        Console.WriteLine(count);
+
+//    }
+//}
