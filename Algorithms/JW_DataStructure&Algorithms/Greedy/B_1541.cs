b1f5e6d74bafef4d2ed2901c68db469029509d71
JaeWoo Joe <joejw7554@gmail.com>
2026-03-03 18:01:04 +0900

    B_1541 풀이 완료

---
+++ Algorithms/JW_DataStructure&Algorithms/Greedy/B_1541.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+//https://www.acmicpc.net/problem/1541
+class B_1541
+{
+    static void Main()
+    {
+        string input = Console.ReadLine();
+
+        string[] separated = input.Split('-');
+
+
+        int sum = Sum(separated[0]);
+
+
+        for (int i = 1; i < separated.Length; i++)
+        {
+            sum -= Sum(separated[i]);
+        }
+
+        Console.WriteLine(sum);
+
+    }
+
+    static int Sum(string group)
+    {
+        int sum = 0;
+        var numbers = group.Split('+');
+
+        foreach (var number in numbers)
+        {
+            sum += int.Parse(number);
+        }
+        return sum;
+    }
+}
+
