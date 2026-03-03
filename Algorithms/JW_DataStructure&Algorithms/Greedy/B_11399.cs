301616e7a701405357403af9dd6e7d520b4655b2
JaeWoo Joe <joejw7554@gmail.com>
2026-03-03 10:18:42 +0900

    B_11399 풀이 완료

---
+++ Algorithms/JW_DataStructure&Algorithms/Greedy/B_11399.cs
@@ -0,0 +1,35 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Text;
+
+////https://www.acmicpc.net/problem/11399
+//class B_11399
+//{
+//    static void Main()
+//    {
+//        //입력받아오는 구간
+//        int size = int.Parse(Console.ReadLine());
+//        string[] inputs = Console.ReadLine().Split();
+//        int[] waitTime = Array.ConvertAll(inputs, int.Parse);
+
+
+//        //오름차순으로 정렬
+//        Array.Sort(waitTime);
+
+
+
+//        //계산방법
+//        //sum 변수를 이용해서 계속 더해서 sum을 중첩시키고 각숫자에 더하여 최소시간을 구한다
+
+//        int prev = 0;
+//        int total = 0;
+//        foreach (int person in waitTime)
+//        {
+//            prev = prev + person; //이전 사람의 시간 + 현재 사람의 시간
+//            total += prev; // 총합 = 이전 사람 + 현재 사람 시간
+//        }
+
+//        Console.WriteLine(total);
+//    }
+
+//}
--- Algorithms/JW_DataStructure&Algorithms/Greedy/B_2839.cs
+++ Algorithms/JW_DataStructure&Algorithms/Greedy/B_2839.cs
@@ -1,29 +1,29 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Text;
 
-//https://www.acmicpc.net/problem/2839
+////https://www.acmicpc.net/problem/2839
 
-class B_2839
-{
-    static void Main()
-    {
-        int input = int.Parse(Console.ReadLine());
+//class B_2839
+//{
+//    static void Main()
+//    {
+//        int input = int.Parse(Console.ReadLine());
 
-        for (int fiveKG = input / 5; fiveKG >= 0; fiveKG--)
-        {
-            int remain = input - (5 * fiveKG);
+//        for (int fiveKG = input / 5; fiveKG >= 0; fiveKG--)
+//        {
+//            int remain = input - (5 * fiveKG);
 
-            if (remain % 3 == 0)
-            {
-                Console.WriteLine(fiveKG + remain / 3);
-                return;
-            }
-        }
-
-
-        Console.WriteLine(-1);
-    }
+//            if (remain % 3 == 0)
+//            {
+//                Console.WriteLine(fiveKG + remain / 3);
+//                return;
+//            }
+//        }
 
 
-}
+//        Console.WriteLine(-1);
+//    }
+
+
+//}
