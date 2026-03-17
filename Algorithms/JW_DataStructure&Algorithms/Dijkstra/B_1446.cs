18a45ede8408879249d32760f392037119455d29
JaeWoo Joe <joejw7554@gmail.com>
2026-03-17 22:35:11 +0900

    B_1446 문제 풀이 완

---
+++ Algorithms/JW_DataStructure&Algorithms/Dijkstra/B_1446.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+//https://www.acmicpc.net/problem/1446
+class B_1446
+{
+    static void Main()
+    {
+        int[] input = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+
+        int N = input[0];
+        int destination = input[1];
+
+        List<(int, int)>[] road = new List<(int, int)>[destination + 1];
+        int[] dist = new int[destination + 1];
+        Array.Fill(dist, int.MaxValue);
+
+        for (int i = 0; i <= destination; i++)
+        {
+            road[i] = new List<(int, int)>();
+            if(i<destination)
+            road[i].Add((i + 1, 1));
+        }
+
+        for (int i = 0; i < N; i++)
+        {
+            int[] input2 = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+
+            int start = input2[0];
+            int end = input2[1];
+            int cost = input2[2];
+
+            if(start<=destination && end <=destination)
+            road[start].Add((end, cost));
+        }
+
+        PriorityQueue<int, int> pq = new PriorityQueue<int, int>();
+
+        dist[0] = 0;
+        pq.Enqueue(0, 0);
+
+        while (pq.Count > 0)
+        {
+            pq.TryDequeue(out int curLocation, out int curCost);
+
+            if (dist[curLocation] < curCost) continue;
+
+            if (curLocation > destination) break;
+
+            foreach (var next in road[curLocation])
+            {
+                int target = next.Item1;
+                int cost = next.Item2;
+                if (curCost + cost < dist[target] && target <= destination)
+                {
+                    dist[target] = curCost + cost;
+                    pq.Enqueue(target, curCost + cost);
+                }
+            }
+        }
+
+        Console.WriteLine(dist[destination]);
+
+
+    }
+}
--- Algorithms/JW_DataStructure&Algorithms/Dijkstra/B_18352.cs
+++ Algorithms/JW_DataStructure&Algorithms/Dijkstra/B_18352.cs
@@ -1,81 +1,81 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Text;
 
-//https://www.acmicpc.net/problem/18352
-class B_18352
-{
-    static void Main()
-    {
-        int[] inputs = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+////https://www.acmicpc.net/problem/18352
+//class B_18352
+//{
+//    static void Main()
+//    {
+//        int[] inputs = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
 
-        int N = inputs[0];
-        int M = inputs[1];
-        int target = inputs[2];
-        int begin = inputs[3];
+//        int N = inputs[0];
+//        int M = inputs[1];
+//        int target = inputs[2];
+//        int begin = inputs[3];
 
-        List<int>[] nodes = new List<int>[N + 1];
+//        List<int>[] nodes = new List<int>[N + 1];
 
-        for (int i = 1; i < N + 1; i++)
-            nodes[i] = new List<int>();
-
-
-        for (int i = 0; i < M; i++)
-        {
-            int[] info = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
-            int from = info[0];
-            int to = info[1];
-            nodes[from].Add(to);
-        }
+//        for (int i = 1; i < N + 1; i++)
+//            nodes[i] = new List<int>();
 
 
-        int[] dist = new int[N + 1];
-        Array.Fill(dist, int.MaxValue);
-        dist[begin] = 0;
-
-        PriorityQueue<int, int> pq = new PriorityQueue<int, int>();
-        pq.Enqueue(begin, 0);
-
-        while (pq.Count > 0)
-        {
-            pq.TryDequeue(out int currentLoc, out int cost);
-
-            if (dist[currentLoc] < cost) continue;
-
-            foreach (var adj in nodes[currentLoc])
-            {
-                if (cost + 1 < dist[adj])
-                {
-                    dist[adj] = cost + 1;
-                    pq.Enqueue(adj, cost + 1);
-                }
-
-            }
-        }
+//        for (int i = 0; i < M; i++)
+//        {
+//            int[] info = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+//            int from = info[0];
+//            int to = info[1];
+//            nodes[from].Add(to);
+//        }
 
 
-        List<int> result = new List<int>();
-        for (int i = 1; i < dist.Length; i++)
-        {
-            if (dist[i] == target)
-            {
-                result.Add(i);
-            }
-        }
+//        int[] dist = new int[N + 1];
+//        Array.Fill(dist, int.MaxValue);
+//        dist[begin] = 0;
 
-        if (result.Count == 0)
-        {
-            Console.WriteLine(-1);
-        }
-        else
-        {
-            foreach (var item in result)
-            {
-                Console.WriteLine(item);
-            }
-        }
+//        PriorityQueue<int, int> pq = new PriorityQueue<int, int>();
+//        pq.Enqueue(begin, 0);
+
+//        while (pq.Count > 0)
+//        {
+//            pq.TryDequeue(out int currentLoc, out int cost);
+
+//            if (dist[currentLoc] < cost) continue;
+
+//            foreach (var adj in nodes[currentLoc])
+//            {
+//                if (cost + 1 < dist[adj])
+//                {
+//                    dist[adj] = cost + 1;
+//                    pq.Enqueue(adj, cost + 1);
+//                }
+
+//            }
+//        }
+
+
+//        List<int> result = new List<int>();
+//        for (int i = 1; i < dist.Length; i++)
+//        {
+//            if (dist[i] == target)
+//            {
+//                result.Add(i);
+//            }
+//        }
+
+//        if (result.Count == 0)
+//        {
+//            Console.WriteLine(-1);
+//        }
+//        else
+//        {
+//            foreach (var item in result)
+//            {
+//                Console.WriteLine(item);
+//            }
+//        }
 
 
 
-    }
-}
+//    }
+//}
