5f6d0cdab884900166f8657f3c5cb6d4b86eceaf
JaeWoo Joe <joejw7554@gmail.com>
2026-04-15 21:14:12 +0900

    B_9095 풀이

--- Algorithms/JW_DataStructure&Algorithms/DP/B_11726.cs
+++ Algorithms/JW_DataStructure&Algorithms/DP/B_11726.cs
@@ -1,37 +1,37 @@
-﻿using System;
-using System.IO;
-using System.Collections.Generic;
-using System.Text;
+﻿//using System;
+//using System.IO;
+//using System.Collections.Generic;
+//using System.Text;
 
-//https://www.acmicpc.net/problem/11726
-class B_11726
-{
-    const int MAX = 1000;
-    static void Main()
-    {
-        //string test = @"9";
-        // StringReader reader = new StringReader(test);
-        using var reader = new StreamReader(Console.OpenStandardInput());
-        StringBuilder sb = new StringBuilder();
+////https://www.acmicpc.net/problem/11726
+//class B_11726
+//{
+//    const int MAX = 1000;
+//    static void Main()
+//    {
+//        //string test = @"9";
+//        // StringReader reader = new StringReader(test);
+//        using var reader = new StreamReader(Console.OpenStandardInput());
+//        StringBuilder sb = new StringBuilder();
 
 
-        string line = reader.ReadLine();
-        if (line != null)
-        {
-            int N = int.Parse(line);
-            int[] memo = new int[Math.Max(N + 1, 3)];
+//        string line = reader.ReadLine();
+//        if (line != null)
+//        {
+//            int N = int.Parse(line);
+//            int[] memo = new int[Math.Max(N + 1, 3)];
 
-            memo[1] = 1;
-            memo[2] = 2;
+//            memo[1] = 1;
+//            memo[2] = 2;
 
-            for (int i = 3; i <= N; i++)
-            {
-                memo[i] = (memo[i - 1] + memo[i - 2]) % 10007;
-            }
+//            for (int i = 3; i <= N; i++)
+//            {
+//                memo[i] = (memo[i - 1] + memo[i - 2]) % 10007;
+//            }
 
-            sb.AppendLine(memo[N].ToString());
+//            sb.AppendLine(memo[N].ToString());
 
-            Console.WriteLine(sb.ToString());
-        }
-    }
-}
+//            Console.WriteLine(sb.ToString());
+//        }
+//    }
+//}
---
+++ Algorithms/JW_DataStructure&Algorithms/DP/B_9095.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Text;
+
+//https://www.acmicpc.net/problem/9095
+class B_9095
+{
+    const int MAX = 10;
+    static void Main()
+    {
+        string test = @"3
+4
+7
+10";
+        //StringReader reader = new StringReader(test);
+        StreamReader reader = new StreamReader(Console.OpenStandardInput());
+        StringBuilder sb = new StringBuilder();
+
+
+        //미리 구하는 구간
+        int[] dp = new int[MAX + 1];
+        dp[1] = 1;
+        dp[2] = 2;
+        dp[3] = 4;
+
+        for (int i = 4; i <= MAX; i++)
+        {
+            dp[i] = dp[i - 1] + dp[i - 2] + dp[i - 3];
+        }
+
+
+        string line = reader.ReadLine();
+        if (line != null)
+        {
+            int T = int.Parse(line);
+
+            while (T > 0)
+            {
+                string Input = reader.ReadLine();
+                if (Input != null)
+                {
+                    int N = int.Parse(Input);
+                    sb.AppendLine(dp[N].ToString());
+                    T--;
+                }
+            }
+        }
+
+        Console.WriteLine(sb.ToString());
+    }
+}
