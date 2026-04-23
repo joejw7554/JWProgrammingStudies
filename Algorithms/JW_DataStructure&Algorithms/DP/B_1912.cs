cb641881d1cbf81094ee8d72132f1ed1d1a7d3b8
JaeWoo Joe <joejw7554@gmail.com>
2026-04-23 20:55:45 +0900

    B_12865

--- Algorithms/JW_DataStructure&Algorithms/DP/B_1912.cs
+++ Algorithms/JW_DataStructure&Algorithms/DP/B_1912.cs
@@ -1,39 +1,39 @@
-﻿using System;
-using System.IO;
-using System.Collections.Generic;
-using System.Text;
+﻿//using System;
+//using System.IO;
+//using System.Collections.Generic;
+//using System.Text;
 
 
-//https://www.acmicpc.net/problem/1912
-class B_1912
-{
-    static void Main()
-    {
-        string test = @"10
-10 -4 3 1 5 6 -35 12 21 -1";
-        StringBuilder sb = new StringBuilder();
-        StringReader reader = new StringReader(test);
+////https://www.acmicpc.net/problem/1912
+//class B_1912
+//{
+//    static void Main()
+//    {
+//        string test = @"10
+//10 -4 3 1 5 6 -35 12 21 -1";
+//        StringBuilder sb = new StringBuilder();
+//        StringReader reader = new StringReader(test);
 
 
-        int N = int.Parse(reader.ReadLine());
+//        int N = int.Parse(reader.ReadLine());
 
-        int[] arr = Array.ConvertAll(reader.ReadLine().Split(), int.Parse);
+//        int[] arr = Array.ConvertAll(reader.ReadLine().Split(), int.Parse);
 
-        int[] DP = new int[N];
+//        int[] DP = new int[N];
 
-        // 0,0 이전 것을 더한것 0,1 새로 시작한 값 
-        DP[0] = arr[0];
-        int Max = arr[0];
+//        // 0,0 이전 것을 더한것 0,1 새로 시작한 값 
+//        DP[0] = arr[0];
+//        int Max = arr[0];
 
-        for (int i = 1; i < N; i++)
-        {
-            DP[i] = Math.Max(arr[i], arr[i] + DP[i - 1]);
-            Max = Math.Max(DP[i], Max);
-        }
+//        for (int i = 1; i < N; i++)
+//        {
+//            DP[i] = Math.Max(arr[i], arr[i] + DP[i - 1]);
+//            Max = Math.Max(DP[i], Max);
+//        }
 
-        sb.AppendLine(Max.ToString());
+//        sb.AppendLine(Max.ToString());
 
-        Console.WriteLine(sb.ToString());
-    }
+//        Console.WriteLine(sb.ToString());
+//    }
 
-}
+//}
---
+++ Algorithms/JW_DataStructure&Algorithms/Dijkstra/B_12865.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Text;
+using System.Data.Common;
+
+//https://www.acmicpc.net/problem/12865
+class B_12865
+{
+    static void Main()
+    {
+        string test = @"4 7
+6 13
+4 8
+3 6
+5 12";
+        //StringReader reader = new StringReader(test);
+        StreamReader reader = new StreamReader(Console.OpenStandardInput());
+
+        StringBuilder sb = new StringBuilder();
+
+        string line = reader.ReadLine();
+        if (line != null)
+        {
+            int[] input = Array.ConvertAll(line.Split(), int.Parse);
+
+            int N = input[0];
+            int K = input[1];
+
+            (int W, int V)[] list = new (int W, int V)[N];
+            for (int i = 0; i < N; i++)
+            {
+                int[] row = Array.ConvertAll(reader.ReadLine().Split(), int.Parse);
+                list[i] = (row[0], row[1]);
+            }
+
+            int[] DP = new int[K + 1];
+
+            for (int i = 0; i < N; i++)
+            {
+                int currentW = list[i].W;
+                int currentV = list[i].V;
+                for (int j = K; j >= currentW; j--)
+                {
+                    DP[j] = Math.Max(DP[j], DP[j - currentW] + currentV);
+                }
+            }
+
+            sb.AppendLine((DP[K]).ToString());
+            Console.WriteLine(sb.ToString());
+
+
+        }
+    }
+
+}
