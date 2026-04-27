fdc0c4e6e4f781d2f36e446888ab626f61db4c79
JaeWoo Joe <joejw7554@gmail.com>
2026-04-27 21:28:45 +0900

    B_14728 풀이완

---
+++ Algorithms/JW_DataStructure&Algorithms/DP/B_14728.cs
@@ -0,0 +1,56 @@
+﻿//using System;
+//using System.IO;
+//using System.Collections.Generic;
+//using System.Text;
+
+
+////https://www.acmicpc.net/problem/14728
+//class B_14728
+//{
+//    static void Main()
+//    {
+//        string test = @"3 310
+//50 40
+//100 70
+//200 150";
+
+//        StringReader reader = new StringReader(test);
+//        //StreamReader reader = new StreamReader(Console.OpenStandardInput());
+//        StringBuilder sb = new StringBuilder();
+
+//        string line = reader.ReadLine();
+
+//        if (line != null)
+//        {
+//            var input = Array.ConvertAll(line.Split(), int.Parse);
+
+//            int n = input[0];
+//            int availableTime = input[1];
+
+//            int[] DP = new int[availableTime + 1];
+//            for (int i = 0; i < n; i++)
+//            {
+//                string[] info = reader.ReadLine().Split();
+//                int require = int.Parse(info[0]);
+//                int score = int.Parse(info[1]);
+
+//                for (int j = availableTime; j >= require; j--)
+//                {
+//                    if (DP[j - require] + score > DP[j])
+//                    {
+//                        DP[j] = DP[j - require] + score;
+//                    }
+//                }
+
+//            }
+
+
+//            sb.AppendLine(DP[availableTime].ToString());
+//        }
+
+//        Console.WriteLine(sb.ToString());
+
+
+//    }
+
+//}
