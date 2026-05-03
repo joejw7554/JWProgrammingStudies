9dafbebd4c8b8249a4f43601adfa2e26a5f9f5f6
JaeWoo Joe <joejw7554@gmail.com>
2026-05-03 22:22:56 +0900

    P_1843_R 풀이완

---
+++ Algorithms/C#/JW_DataStructure&Algorithms/DP/P_1843_R.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Numerics;
+using System.Text;
+
+
+//https://school.programmers.co.kr/learn/courses/30/lessons/1843
+class P_1843_R
+{
+    int solution(string[] arr)
+    {
+        int answer = -1;
+
+
+        //단계1 데이터 분리
+        List<int> numbers = new List<int>();
+        List<char> opers = new List<char>();
+
+        foreach (string str in arr)
+        {
+            if (int.TryParse(str, out int num))
+            {
+                numbers.Add(num);
+            }
+            else
+            {
+                opers.Add(str[0]);
+            }
+        }
+
+        int[][] max_dp = new int[numbers.Count][];
+        int[][] min_dp = new int[numbers.Count][];
+
+        for (int i = 0; i < max_dp.Length; i++)
+        {
+            max_dp[i] = new int[numbers.Count];
+            min_dp[i] = new int[numbers.Count];
+
+            max_dp[i][i] = numbers[i];
+            min_dp[i][i] = numbers[i];
+        }
+
+        //단계 2: 루프 구성
+        int n = numbers.Count;
+        for (int step = 1; step < n; step++)
+        {
+            for (int i = 0; i < n - step; i++)
+            {
+                int j = step + i;
+
+                max_dp[i][j] = int.MinValue;
+                min_dp[i][j] = int.MaxValue;
+
+                for (int k = i; k < j; k++)
+                {
+                    Console.WriteLine($"구간[{i}~{j}] 계산 중... 기준점 k={k} ({numbers[i]}...{numbers[j]})");
+                    Console.WriteLine($"  -> 왼쪽({i}~{k}): {max_dp[i][k]}, 오른쪽({k + 1}~{j}): {max_dp[k + 1][j]}");
+
+                    if (opers[k] == '+')
+                    {
+                        max_dp[i][j] = Math.Max(max_dp[i][j], max_dp[i][k] + max_dp[k + 1][j]);
+                        min_dp[i][j] = Math.Min(min_dp[i][j], min_dp[i][k] + min_dp[k + 1][j]);
+                    }
+                    else if (opers[k] == '-')
+                    {
+                        max_dp[i][j] = Math.Max(max_dp[i][j], max_dp[i][k] - min_dp[k + 1][j]);
+                        min_dp[i][j] = Math.Min(min_dp[i][j], min_dp[i][k] - max_dp[k + 1][j]);
+                    }
+                }
+
+            }
+        }
+
+
+        //단계 3: 최종결과
+        return answer;
+    }
+
+    static void Main()
+    {
+        P_1843_R test = new P_1843_R();
+        string[] sample = new string[] { "1", "-", "3", "+", "5", "-", "8" };
+
+        var result = test.solution(sample);
+    }
+}
--- Algorithms/C#/JW_DataStructure&Algorithms/Experiment/ListMemoryReferenceTest.cs
+++ Algorithms/C#/JW_DataStructure&Algorithms/Experiment/ListMemoryReferenceTest.cs
@@ -1,20 +1,20 @@
-﻿using System;
-using System.Collections.Generic;
+﻿//using System;
+//using System.Collections.Generic;
 
-class ListMemoryReferenceTest
-{
-    static void Main()
-    {
-        // 1. 외부 리스트 생성
-        List<List<int>> outerList = new List<List<int>>();
+//class ListMemoryReferenceTest
+//{
+//    static void Main()
+//    {
+//        // 1. 외부 리스트 생성
+//        List<List<int>> outerList = new List<List<int>>();
 
-        // 2. 내부 리스트 생성 및 데이터 삽입
-        List<int> innerList = new List<int>();
-        innerList.Add(777); // 우리가 찾을 목표 데이터: 777
+//        // 2. 내부 리스트 생성 및 데이터 삽입
+//        List<int> innerList = new List<int>();
+//        innerList.Add(777); // 우리가 찾을 목표 데이터: 777
 
-        outerList.Add(innerList);
+//        outerList.Add(innerList);
 
-        // 여기서 중단점(F9)을 걸고 조사를 시작합니다.
-        Console.WriteLine("조사 시작!");
-    }
-}
+//        // 여기서 중단점(F9)을 걸고 조사를 시작합니다.
+//        Console.WriteLine("조사 시작!");
+//    }
+//}
