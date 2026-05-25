f1f1cfe27974fcad1774ad23a1d67f312b30bae9
JaeWoo Joe <joejw7554@gmail.com>
2026-05-25 14:22:26 +0900

    P_42840 풀이완

---
+++ Algorithms/C#/JW_DataStructure&Algorithms/BruteForce/1/P_42840.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Xml.XPath;
+
+//https://school.programmers.co.kr/learn/courses/30/lessons/42840?language=csharp
+class P_42840
+{
+    public int[] solution(int[] answers)
+    {
+        List<int> answer = new List<int>();
+        int[] result = new int[3];
+
+        int n = answers.Length;
+
+        //전체 진행
+        //일단 1,2,3 맞춘 갯수를 기록한다 이과정에서 Max값이 새로나오면 갱신한다
+        //기록에서 Max값의 인덱스를 출력한다
+
+        int[] pattern1 = new int[] { 1, 2, 3, 4, 5 };
+        int[] pattern2 = new int[] { 2, 1, 2, 3, 2, 4, 2, 5 };
+        int[] pattern3 = new int[] { 3, 3, 1, 1, 2, 2, 4, 4, 5, 5 };
+
+
+        for (int i = 0; i < n; i++)
+        {
+            if (answers[i] == pattern1[i % pattern1.Length]) result[0]++;
+            if (answers[i] == pattern2[i % pattern2.Length]) result[1]++;
+            if (answers[i] == pattern3[i % pattern3.Length]) result[2]++;
+        }
+
+
+        int max = result.Max();
+        for (int i = 0; i < result.Length; i++)
+        {
+            if (result[i] == max) answer.Add(i + 1);
+        }
+
+        return answer.ToArray();
+    }
+
+
+    static void Main()
+    {
+
+    }
+
+}
---
+++ Algorithms/C#/JW_DataStructure&Algorithms/BruteForce/1/P_86491.cs
@@ -0,0 +1,36 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Text;
+
+////https://school.programmers.co.kr/learn/courses/30/lessons/86491
+//class P_86491
+//{
+//    public int solution(int[,] sizes)
+//    {
+//        int maxWidth = 0;
+//        int maxHeight = 0;
+
+//        for (int i = 0; i < sizes.GetLength(0); i++)
+//        {
+//            int currentMax = Math.Max(sizes[i, 0], sizes[i, 1]);
+//            int currentMin = Math.Min(sizes[i, 0], sizes[i, 1]);
+
+//            maxWidth = Math.Max(currentMax, maxWidth);
+//            maxHeight = Math.Max(currentMin, maxHeight);
+
+//        }
+
+//        int answer = maxWidth * maxHeight;
+//        return answer;
+//    }
+
+
+//    static void Main()
+//    {
+//        var test = new P_86491();
+//        var result = test.solution(new int[,] { { 60, 50 }, { 30, 70 }, { 60, 30 }, { 80, 40 } });
+//        Console.WriteLine(result);
+
+
+//    }
+//}
